69d464f3b5f867b98b0d082bc4003539c764b409
bpUX <brainpowerux@gmail.com>
2015-10-07 22:45:01 -0400

    added enum and some operation logic

--- 8bit-calc/ViewController.swift
+++ 8bit-calc/ViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // ENUMS
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
     // PROPERTIES
     var btnSound: AVAudioPlayer!
     var runningNumber = ""
     var leftValStr = ""
     var rightValStr = ""
-    var operation = ""
+    var currentOperation: Operation = Operation.Empty
     
     // OUTLETS
     @IBOutlet weak var outputLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
         let soundURL = NSURL(fileURLWithPath: path!)
         
@@ -36,24 +48,53 @@ class ViewController: UIViewController {
     
     // know which button was pressed
     @IBAction func numberPressed(btn: UIButton!) {
+        playSound()
         
-        btnSound.play()
+        if runningNumber.characters.count < 10 {
+            runningNumber += "\(btn.tag)"
+            outputLbl.text = runningNumber
+        }
     }
     
     @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
     }
     
     @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
     }
     
     @IBAction func onMinusPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
     }
     
     @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
     }
     
-    
     @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(Operation.Equals)
+    }
+    
+    func processOperation(op: Operation){
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            // Run some math
+        } else {
+            // This is the first time the operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound(){
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
     }
     
 }
